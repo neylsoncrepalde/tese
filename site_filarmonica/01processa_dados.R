69fa90af16c62ecac3a3f0bfbc0bafca36cd9ebf
neylsoncrepalde <neylsoncrepalde@gmail.com>
2018-01-08 11:11:14 +0100

    inicia base compositores

--- site_filarmonica/01processa_dados.R
+++ site_filarmonica/01processa_dados.R
@@ -7,16 +7,35 @@ library(dplyr)
 library(tidyr)
 library(readr)
 
-fil2016 = read_csv("concertos_filarmonica_2016.csv")
-names(fil2016)
+fil2016 = read_csv("concertos_filarmonica_2016_corrigido.csv")
+fil2015 = read_csv("concertos_filarmonica_2015_corrigido.csv")
+fil2014 = read_csv("concertos_filarmonica_2014_corrigido.csv")
+fil2013 = read_csv("concertos_filarmonica_2013_corrigido.csv")
+fil2012 = read_csv("concertos_filarmonica_2012_corrigido.csv")
+fil2011 = read_csv("concertos_filarmonica_2011_corrigido.csv")
+fil2010 = read_csv("concertos_filarmonica_2010_corrigido.csv")
+fil2009 = read_csv("concertos_filarmonica_2009_corrigido.csv")
+fil2008 = read_csv("concertos_filarmonica_2008_corrigido.csv")
+
+# Junta todos num banco de dados completo
+fil_completo = rbind(fil2016, fil2015, fil2014, fil2013, fil2012, fil2011,
+                     fil2010, fil2009, fil2008) #OK
 
 # Separa compositor e obra
-fil2016 = fil2016 %>% separate(repertorio, c("compositor", "obra"), sep = "\\|")
+fil_completo = fil_completo %>% 
+  separate(repertorio, c("compositor", "obra"), sep = "[[:space:]]\\|")
+
+# Exporta um csv com compositores para classificação de estilo
+fil_completo %>% select(compositor) %>% unique %>% arrange(compositor) %>%
+  write_excel_csv(., "fil_compositores.csv") #OK
+
 
 # Separa os dias
-fil2016 = fil2016 %>% mutate(V2 = strsplit(dia, " e ")) %>% unnest
+fil_completo = fil_completo %>% mutate(V2 = strsplit(dia, " e ")) %>% unnest
+
+
 
 # Exporta um csv com ano, mes e dias separados para juntar com 
 # taxa de ocupação
-fil2016 %>% select(ano, mes, V2) %>% unique %>% 
+fil_completo %>% select(ano, mes, V2) %>% unique %>% 
   write_excel_csv(., "occupancy_2016.csv")
