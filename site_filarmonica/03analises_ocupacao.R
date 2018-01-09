8e4f2bfb4fd455ab9b77814bc7b2a8680117b543
neylsoncrepalde <neylsoncrepalde@gmail.com>
2018-01-09 11:14:26 +0100

    predict tx_ocup 2015

--- site_filarmonica/03analises_ocupacao.R
+++ site_filarmonica/03analises_ocupacao.R
@@ -26,19 +26,56 @@ fil_ocupacao = left_join(fil_completo, ocupacao)
 ### ANÁLISES
 View(fil_ocupacao)
 
+# 2016
 fil_2016 = fil_ocupacao %>% filter(ano == 2016)
 fil_2016$tx_ocup = (fil_2016$publico / fil_2016$pot_ocup) * 100
-summary(fil_2016$tx_ocup)
+summary(fil_2016$tx_ocup[fil_2016$local == "Sala Minas Gerais"])
 
 analise_variancia = aov(formula = tx_ocup ~ periodo, data = fil_2016)
 summary(analise_variancia)
 
-salaminasgerais = fil_2016 %>% filter(local == "Sala Minas Gerais")
+#Exclui a quarta pq só tem 5 casos
+fil_2016 = fil_2016 %>% filter(dia_semana != "quarta")
+freq(fil_2016$dia_semana)
 
-fit1 = lm(tx_ocup ~ periodo + dia_semana, 
-          data = salaminasgerais)
+fit1 = lm(tx_ocup ~ relevel(factor(periodo), "ROMANTISMO") + 
+            relevel(factor(dia_semana), "quinta"), 
+          data = fil_2016)
 summary(fit1)
 car::vif(fit1)
 texreg::screenreg(fit1)
 
-freq(fil_2016$dia_semana)
+# Verificando a média de lugares disponíveis na sala minas gerais
+SMG2016 = fil_2016 %>% filter(local == "Sala Minas Gerais") %>%
+  group_by(local) %>% summarise(mean = mean(pot_ocup, na.rm = T), 
+                                sd = sd(pot_ocup, na.rm = T),
+                                median = median(pot_ocup, na.rm = T))
+
+# 2015
+fil_2015 = fil_ocupacao %>% filter(ano == 2015, dia_semana != "quarta",
+                                   local == "Sala Minas Gerais",
+                                   serie != "Especial")
+SMG2015 = fil_2015 %>% filter(local == "Sala Minas Gerais") %>%
+  group_by(local) %>% summarise(mean = mean(publico, na.rm = T), 
+                                sd = sd(publico, na.rm = T),
+                                median = median(publico, na.rm = T))
+
+# Razão entre médias
+ocupacao_media_2015 = fil_2015$publico / SMG2016$mean
+ocupacao_mediana_2015 = fil_2015$publico / SMG2016$median
+
+mediadasduas = apply(cbind(ocupacao_media_2015, ocupacao_mediana_2015),
+                     1, mean)
+
+
+# Investigando a lógica de oferta de lugares na sala minas gerais em 2016
+# "Antedizendo" para 2016
+fit.oferta = lm(pot_ocup ~ serie + relevel(factor(dia_semana), "quinta"), 
+                fil_2016)
+summary(fit.oferta)
+car::vif(fit.oferta)
+
+pot_ocup_chapeu = predict.lm(fit.oferta, newdata = fil_2015) %>% round
+
+
+
