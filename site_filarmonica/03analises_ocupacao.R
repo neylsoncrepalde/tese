b2fe221e4f9958f38b9c2ae0088e5ba3eed3e7c4
neylsoncrepalde <neylsoncrepalde@gmail.com>
2018-01-09 19:59:04 +0100

    predict pot_ocup multinomial ok

--- site_filarmonica/03analises_ocupacao.R
+++ site_filarmonica/03analises_ocupacao.R
@@ -71,50 +71,59 @@ mediadasduas = apply(cbind(ocupacao_media_2015, ocupacao_mediana_2015),
 # Investigando a lógica de oferta de lugares na sala minas gerais em 2016
 # "Antedizendo" para 2016
 
-# Usando regressão linear
-fit.oferta = lm(pot_ocup ~ serie + relevel(factor(dia_semana), "quinta"), 
-                fil_2016)
-summary(fit.oferta)
-plot(fit.oferta)
-car::vif(fit.oferta)
+# Usando regressão logística multinomial
+library(nnet)
+library(caret)
+fil_2016 = fil_2016 %>% filter(!is.na(pot_ocup))
+fil_2016$pot_ocup = as.factor(fil_2016$pot_ocup)
 
-pot_ocup_chapeu = predict.lm(fit.oferta, newdata = fil_2015) %>% round
-mean(pot_ocup_chapeu)
+train.multi = sample(1:nrow(fil_2016), nrow(fil_2016) * .7)
+fit.oferta = multinom(factor(pot_ocup) ~ factor(serie) + 
+                  relevel(factor(dia_semana), "quinta") + publico, 
+                fil_2016, model = T, subset = train.multi)
+summary(fit.oferta)
+# Testando o modelo
+pred.multi = predict(fit.oferta, fil_2016[-train.multi,])
+confusion.multi = table(pred.multi, fil_2016$pot_ocup[-train.multi])
+confusionMatrix(confusion.multi)
+
+pot_ocup_chapeu = predict(fit.oferta, newdata = fil_2015) # BOM!
 
 
 # Tentando construir um modelo com Random Forests
 library(randomForest)
+library(RFinfer)
 rf.dataset = fil_2016 %>% 
-  select(pot_ocup, serie, dia_semana, periodo) %>%
+  select(pot_ocup, serie, dia_semana, periodo, publico) %>%
   filter(complete.cases(.) == T)
 
 rf.dataset$serie = as.factor(rf.dataset$serie)
 rf.dataset$dia_semana = as.factor(rf.dataset$dia_semana)
 rf.dataset$periodo = as.factor(rf.dataset$periodo)
-rf.dataset$pot_ocup = as.factor(rf.dataset$pot_ocup)
 
 train = sample(1:nrow(rf.dataset), nrow(rf.dataset) * .8)
 
-Xtrain = model.matrix(pot_ocup ~ serie + dia_semana + periodo, 
+Xtrain = model.matrix(pot_ocup ~ serie + dia_semana + periodo + publico, 
                  data = rf.dataset[train,])
 Ytrain = rf.dataset$pot_ocup[train]
   
-Xtest = model.matrix(pot_ocup ~ serie + dia_semana + periodo, 
+Xtest = model.matrix(pot_ocup ~ serie + dia_semana + periodo + publico, 
                      data = rf.dataset[-train,])
 Ytest = rf.dataset$pot_ocup[-train]
 
 
 rf.fit = randomForest(x = Xtrain, y = Ytrain, xtest = Xtest, ytest = Ytest, 
                       mtry = 3, data = rf.dataset, importance = T,
-                      ntree = 10000, keep.forest = T)
+                      ntree = 10000, keep.forest = T, keep.inbag = T)
 rf.fit
+
 
 # Testando o erro
 oob.err = double(3)
 oob.test = double(3)
 for(mtry in 1:3){
   fit = randomForest(x = Xtrain, y = Ytrain, xtest = Xtest, ytest = Ytest, 
-                   mtry = 3, data = rf.dataset, importance = T)
+                   mtry = mtry, data = rf.dataset, importance = T)
   oob.err[mtry] = fit$err.rate[500]
   oob.test[mtry] = fit$test$err.rate[500]
   cat(mtry," ")
@@ -127,13 +136,13 @@ cbind(oob.err,oob.test)
 
 # Predição para 2015
 pred.2015 = fil_2015 %>% 
-  select(ano, mes, publico, serie, dia_semana, periodo) %>%
-  filter(complete.cases(.) == T)
+  select(ano, mes, publico, serie, dia_semana, periodo)
 pred.2015$serie = factor(pred.2015$serie)
 pred.2015$dia_semana = factor(pred.2015$dia_semana)
 pred.2015$periodo = factor(pred.2015$periodo)
 
-predict(rf.fit, pred.2015)
+X2015 = model.matrix(ano~serie + dia_semana + periodo, data = pred.2015)
+X2015
 
 
 
