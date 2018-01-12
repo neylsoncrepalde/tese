d406c0bd15376d1632488ea664dddb1bd2c2a971
neylsoncrepalde <neylsoncrepalde@gmail.com>
2018-01-12 04:49:58 +0100

    cria script

---
+++ site_filarmonica/04primeiras_analises.R
@@ -0,0 +1,7 @@
+# 4 - Primeiras analises
+# Orquestra Filarmônica
+# Tese - Neylson Crepalde
+##########################
+
+library(readr)
+filarmonica = read_csv("concertos_filarmonica_ocupacao1516.csv")
