c03cf157ad6e26e04c811b305a45bb3134b24137
Neylson Crepalde <neylsoncrepalde@gmail.com>
2019-01-05 15:10:11 -0200

    acrescenta redes Ouro Preto e SESI

--- monta_redes.R
+++ monta_redes.R
@@ -10,7 +10,7 @@ library(xtable)
 
 filarmonica = fread('~/tese_files/filarmonica_network.csv', header = T) %>% as_tibble %>% 
   select(1:4) %>% slice(1:(n()-2))
-filarmonica %>% tail
+filarmonica %>% View
 
 
 gfil = graph_from_edgelist(as.matrix(filarmonica[1:2]), directed = F)
@@ -70,6 +70,36 @@ png('rede_opus.png', height = 600, width = 600)
 plot(gopus, vertex.shape = "none", edge.color = as.factor(E(gopus)$relation),
      vertex.label.color = as.integer(as.factor(V(gopus)$cor)))
 dev.off()
+
+## Ouro Preto ####
+ouropreto = fread('~/tese_files/ouro_preto_network.csv', header = T) %>% as_tibble %>% 
+  select(1:4)
+ouropreto
+
+gouropreto = graph_from_edgelist(as.matrix(ouropreto[1:2]), directed = F)
+E(gouropreto)$relation = ouropreto$relation
+
+V(gouropreto)$cor = c("z", ouropreto$relation)
+
+plot(gouropreto, vertex.shape = "none", edge.color = as.factor(E(gouropreto)$relation),
+     vertex.label.color = as.integer(as.factor(V(gouropreto)$cor)))
+
+png('rede_ouropreto.png', height = 600, width = 600)
+plot(gouropreto, vertex.shape = "none", edge.color = as.factor(E(gouropreto)$relation),
+     vertex.label.color = as.integer(as.factor(V(gouropreto)$cor)))
+dev.off()
+
+## SESI ####
+sesi = fread('~/tese_files/sesi_network.csv', header = T) %>% as_tibble %>% 
+  select(1:4)
+sesi
+
+gsesi = graph_from_edgelist(as.matrix(sesi[1:2]), directed = F)
+E(gsesi)$relation = sesi$relation
+
+V(gsesi)$cor = c("z", sesi$relation)
+
+
 
 
 # Teste rede completa
