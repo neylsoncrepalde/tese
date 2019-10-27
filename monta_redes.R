a539cb3f23206801624af9311a4294da826d88cc
Neylson Crepalde <neylsoncrepalde@Neylsons-MacBook-Air.local>
2019-10-27 17:18:50 -0300

    consertei os dados das orquestras

--- monta_redes.R
+++ monta_redes.R
@@ -9,14 +9,14 @@ library(xtable)
 ## Filarmônica ####
 
 filarmonica = fread('~/tese_files/filarmonica_network.csv', header = T) %>% as_tibble %>% 
-  select(1:4) %>% slice(1:(n()-2))
+  select(1:2) #%>% slice(1:(n()-2))
 filarmonica %>% tail
 
 
 gfil = graph_from_edgelist(as.matrix(filarmonica[1:2]), directed = F)
-E(gfil)$relation = filarmonica$relation
+#E(gfil)$relation = filarmonica$relation
 
-V(gfil)$cor = c("z", filarmonica$relation)
+#V(gfil)$cor = c("z", filarmonica$relation)
 
 # plot(gfil, vertex.shape = "none", edge.color = as.factor(E(gfil)$relation),
 #      vertex.label.color = as.integer(as.factor(V(gfil)$cor)))
@@ -26,24 +26,24 @@ V(gfil)$cor = c("z", filarmonica$relation)
 #      vertex.label.color = as.integer(as.factor(V(gfil)$cor)))
 # dev.off()
 
-filarmonica %>% group_by(relation) %>% summarise(n=n()) %>% 
-  arrange(desc(n)) %>% xtable %>% print.xtable(include.rownames = F)
+#filarmonica %>% group_by(relation) %>% summarise(n=n()) %>% 
+#  arrange(desc(n)) %>% xtable %>% print.xtable(include.rownames = F)
 
-filarmonica %>% filter(relation == "parceria institucional")
+#filarmonica %>% filter(relation == "parceria institucional")
 
-filarmonica %>% filter(relation == "patrocinador") %>% group_by(activity) %>% summarise(n = n()) %>% 
-  arrange(desc(n))
+#filarmonica %>% filter(relation == "patrocinador") %>% group_by(activity) %>% summarise(n = n()) %>% 
+#  arrange(desc(n))
 
 ## Sinfônica ####
 
 sinfonica = fread('~/tese_files/sinfonica_network.csv', header = T) %>% as_tibble %>% 
-  select(1:4)
+  select(1:2)
 sinfonica
 
 gsinf = graph_from_edgelist(as.matrix(sinfonica[1:2]), directed = F)
-E(gsinf)$relation = sinfonica$relation
+#E(gsinf)$relation = sinfonica$relation
 
-V(gsinf)$cor = c("z", sinfonica$relation)
+#V(gsinf)$cor = c("z", sinfonica$relation)
 
 # plot(gsinf, vertex.shape = "none", edge.color = as.factor(E(gsinf)$relation),
 #      vertex.label.color = as.integer(as.factor(V(gsinf)$cor)))
@@ -55,13 +55,13 @@ V(gsinf)$cor = c("z", sinfonica$relation)
 
 ## OPUS ####
 opus = fread('~/tese_files/opus_network.csv', header = T) %>% as_tibble %>% 
-  select(1:4)
+  select(1:2)
 opus
 
 gopus = graph_from_edgelist(as.matrix(opus[1:2]), directed = F)
-E(gopus)$relation = opus$relation
+#E(gopus)$relation = opus$relation
 
-V(gopus)$cor = c("z", opus$relation)
+#V(gopus)$cor = c("z", opus$relation)
 
 # plot(gopus, vertex.shape = "none", edge.color = as.factor(E(gopus)$relation),
 #      vertex.label.color = as.integer(as.factor(V(gopus)$cor)))
@@ -73,13 +73,13 @@ V(gopus)$cor = c("z", opus$relation)
 
 ## Ouro Preto ####
 ouropreto = fread('~/tese_files/ouro_preto_network.csv', header = T) %>% as_tibble %>% 
-  select(1:4)
+  select(1:2)
 ouropreto
 
 gouropreto = graph_from_edgelist(as.matrix(ouropreto[1:2]), directed = F)
-E(gouropreto)$relation = ouropreto$relation
+#E(gouropreto)$relation = ouropreto$relation
 
-V(gouropreto)$cor = c("z", ouropreto$relation)
+#V(gouropreto)$cor = c("z", ouropreto$relation)
 
 # plot(gouropreto, vertex.shape = "none", edge.color = as.factor(E(gouropreto)$relation),
 #      vertex.label.color = as.integer(as.factor(V(gouropreto)$cor)))
@@ -90,14 +90,14 @@ V(gouropreto)$cor = c("z", ouropreto$relation)
 # dev.off()
 
 ## SESI ####
-sesi = fread('~/tese_files/sesi_network.csv', header = T) %>% as_tibble %>% 
-  select(1:4)
+sesi = fread('~/tese_files/sesiminas_network.csv', header = T) %>% as_tibble %>% 
+  select(1:2)
 sesi
 
 gsesi = graph_from_edgelist(as.matrix(sesi[1:2]), directed = F)
-E(gsesi)$relation = sesi$relation
+#E(gsesi)$relation = sesi$relation
 
-V(gsesi)$cor = c("z", sesi$relation)
+#V(gsesi)$cor = c("z", sesi$relation)
 
 
 
@@ -114,4 +114,4 @@ gnivel2 = gfil %u% gsinf %u% gouropreto %u% gsesi %u% gopus
 V(gnivel2)$type = TRUE
 
 
- 
+plot(gnivel2, vertex.size=3)
